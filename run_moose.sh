fde8a0f2380572bcee89a8148239535d5dbe7cc5
Daniel Schwen <daniel@schwen.de>
2018-02-05 15:36:09 -0500

    add timeout, sanitize args

--- run_moose.sh
+++ run_moose.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# unprivileged UID to run under
+MYUID=1002
+MYGID=1002
+
 # source MOOSE profile
 echo Loading MOOSE environment...
 . /opt/moose/environments/moose_profile
 
-# overlayfs name
-NAME=test
+# overlayfs name (sanitize for security)
+NAME=${1//[^a-zA-Z0-9_]/}
 
 # overlay directories
 OVER=/var/overlay
@@ -13,13 +17,11 @@ WORK=$OVER/work/$NAME
 UPPER=$OVER/upper/$NAME
 MNT=$OVER/mnt/$NAME
 
-
 # chroot lower
 LOWER=/var/chroot/moose
 
-# make directories and mount points
+# make directories and mount points (UPPER is created by the webserver)
 mkdir -p $WORK
-mkdir -p $UPPER
 mkdir -p $MNT
 
 # mount overlay
@@ -28,9 +30,8 @@ mount -t overlay overlay -o lowerdir=$LOWER,upperdir=$UPPER,workdir=$WORK $MNT
 
 # run moose
 echo Running MOOSE...
-chroot --userspec daniel:daniel /var/chroot/moose /home/daniel/moose/modules/combined/combined-opt -i test.i
-echo $UPPER
+timeout 120s chroot --userspec $MYUID:$MYGID $MNT /home/daniel/moose/modules/combined/combined-opt -i test.i
 
 # unmount overlayfs
 echo Unmounting overlay file system...
-#umount $MNT
+umount $MNT
